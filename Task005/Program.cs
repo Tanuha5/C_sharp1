db3674e90f4ed00bd1a65eb7d5e20c71684da558
Tatiana <Tanuha5@rambler.ru>
2023-02-05 15:00:17 +0300

    Проверка на трехзначность

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -8,4 +8,13 @@
 
 System.Console.WriteLine("Введите трехзначное число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine(N%10);
+
+if (N > 99 && N < 1000)
+{
+    System.Console.WriteLine("Последняя цифра: " + N%10);
+}
+else
+{
+    System.Console.WriteLine("Введено не трехзначное число");
+}
+
